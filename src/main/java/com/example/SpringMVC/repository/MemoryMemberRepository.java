892346ce8a2dafe7be80a31d9018d324171dba63
ulsandonghun <donghoon141@naver.com>
2024-01-10 17:38:09 +0900

    [test] : 회원 가입 서비스 Test코드 구현

--- src/main/java/com/example/SpringMVC/repository/MemoryMemberRepository.java
+++ src/main/java/com/example/SpringMVC/repository/MemoryMemberRepository.java
@@ -9,6 +9,11 @@ import java.util.Optional;
 
 public class MemoryMemberRepository implements MemberRepository {
 
+
+    //문제상황 여기서는 실질적으로 저장되고 있는 Map은 Static이라서 클래스에 붙
+    //어있는것이라 인스턴스가 바뀌어도 같은값이지만 나약 static이 아닌경우
+    //같은 레포지토리 인스턴스를 호출하지 않으면 엉뚱한 DB를 호출하는 꼴이
+    //되기때문에 생성자로 하나의 레포지토리만 쓰도록 Service에서 강제한다.
     private static Map<Long, Member> store = new HashMap<>();
     private static long sequence = 0L;
     @Override
---
+++ src/test/java/com/example/SpringMVC/service/MemberServiceTest.java
@@ -0,0 +1,79 @@
+package com.example.SpringMVC.service;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.example.SpringMVC.domain.Member;
+import com.example.SpringMVC.repository.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MemberServiceTest {
+
+    MemberService memberService;
+    MemoryMemberRepository memberRepository;
+
+    @BeforeEach
+    public void setDB() {
+        memberRepository=new MemoryMemberRepository()
+        memberService = new MemberService(memberRepository);
+    }
+
+
+    @AfterEach
+    void clearDB() {
+        memberRepository.clearStore();
+    }
+
+    @Test
+    void 회원가입() {
+
+        //given
+        Member member = new Member();
+        member.setName("hello");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        Member findMember = memberService.findOne(saveId).get();
+
+        assertThat(saveId).isEqualTo(findMember.getId());
+
+    }
+
+    @Test
+    public void 중복_회원_예제() {
+        //given
+        Member member1 = new Member();
+        member1.setName("최동훈");
+
+        Member member2 = new Member();
+        member2.setName("최동훈");
+
+        //when
+
+        //예외 발생 첫방법
+//        memberService.join(member1);
+//        try {
+//            memberService.join(member2);
+//            fail();
+//
+//        } catch (IllegalStateException e) {
+//            assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다");
+//        }
+        //추천 바업ㅂ
+        memberService.join(member1);
+        assertThrows(IllegalStateException.class,()->memberService.join(member2));
+    }
+
+    @Test
+    void 회원조회() {
+    }
+
+    @Test
+    void findOne() {
+    }
+}
