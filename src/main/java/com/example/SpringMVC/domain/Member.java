5af640e11066aa3c41fbc22bb73bc4ae5b5dc301
ulsandonghun <donghoon141@naver.com>
2024-01-10 16:29:39 +0900

    [feat] : 회원 저장 기능 구현

---
+++ src/main/java/com/example/SpringMVC/domain/Member.java
@@ -0,0 +1,12 @@
+package com.example.SpringMVC.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Member {
+    private Long id;
+    private String name;
+
+}
