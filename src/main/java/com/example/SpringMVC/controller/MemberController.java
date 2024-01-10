e81d3aa13dfb8a8dec10ece792d8c0bb491d95e1
ulsandonghun <donghoon141@naver.com>
2024-01-10 20:33:05 +0900

    [feat]: 회원가입 페이지 구현

--- src/main/java/com/example/SpringMVC/controller/MemberController.java
+++ src/main/java/com/example/SpringMVC/controller/MemberController.java
@@ -1,9 +1,14 @@
 package com.example.SpringMVC.controller;
 
 
+import com.example.SpringMVC.domain.Member;
 import com.example.SpringMVC.service.MemberService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -18,4 +23,24 @@ public class MemberController {
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
+
+    @GetMapping(value = "/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping(value = "/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+        memberService.join(member);
+        return "redirect:/";
+    }
+
+    @GetMapping(value = "/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }g
 }
